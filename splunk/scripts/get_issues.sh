3438fd6bf344ad79f51fd8f23f2da9a76e2c64b0
gassara-kys <ogasawara_kiyoshi@cyberagent.co.jp>
2017-09-07 20:20:55 +0900

    splunkからRedmineDB参照できるようにスクリプト追加

--- splunk/scripts/get_issues.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-curl -s -XGET http://redmine:3000/issues.json \
-| jq '.issues[] | {id : .id, tracker : .tracker.name, status: .status.name, category: .category.name, suject : .subject, updated_date: .updated_on, created_date:.created_on }| select(.updated_date > "2017-08-31T03:00:00Z")'
